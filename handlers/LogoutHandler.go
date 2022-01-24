7fb89c1cecc9af7de13a4abc85ca7c3a41054b09
NLForces <btimm3rar3nds@gmail.com>
2022-01-24 21:07:11 +0100

    2.1

--- handlers/LogoutHandler.go
+++ handlers/LogoutHandler.go
@@ -1,13 +1,20 @@
 package handlers
 
 import (
+	"html/template"
 	"log"
 	"net/http"
 )
 
 func LogoutHandler(response http.ResponseWriter, request *http.Request) {
 	log.Println("Logged out")
-
 	http.SetCookie(response, nil)
-	http.Redirect(response, request, "/login", http.StatusFound)
+
+	render, err := template.ParseFiles("templates/list-transactions.html")
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	render.Execute(response, nil)
 }
