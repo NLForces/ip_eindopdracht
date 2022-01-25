82e7c31312ada0d63360988494ef682783666e92
NLForces <btimm3rar3nds@gmail.com>
2022-01-25 16:53:47 +0100

    2.7

--- handlers/CreateAccountHandler.go
+++ handlers/CreateAccountHandler.go
@@ -38,7 +38,6 @@ func CreateAccountPostHandler(response http.ResponseWriter, request *http.Reques
 	code := "IBAN " + (request.FormValue("code"))
 	maxcredit, _ := strconv.ParseFloat(request.FormValue("maxcredit"), 64)
 	pincode := (request.FormValue("pincode"))
-	log.Println(pincode)
 
 	var cancreate bool = repositories.CanCreateAccount(name, code)
 
