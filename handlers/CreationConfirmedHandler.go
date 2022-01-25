01c1a9c14606f1da2c4c64b5d4990f805280fd59
NLForces <btimm3rar3nds@gmail.com>
2022-01-25 17:33:02 +0100

    2.8

--- handlers/CreationConfirmedHandler.go
+++ handlers/CreationConfirmedHandler.go
@@ -1,32 +1,30 @@
 package handlers
 
 import (
+	"fmt"
 	"html/template"
 	"log"
 	"main/repositories"
 	"net/http"
+	"strconv"
 )
 
 func CreationConfirmedHandler(response http.ResponseWriter, request *http.Request) {
 	log.Println("CreationConfirmed")
 
 	allAccounts := repositories.GetAllAccounts()
-	account_id := allAccounts[len(allAccounts)-1].ID //Roept het ID op van het laatste account
+	account_id := strconv.FormatUint(uint64(allAccounts[len(allAccounts)-1].ID), 10) //Roept het ID op van het laatste account en convert dit in een string
 	name := allAccounts[len(allAccounts)-1].Name
 	code := allAccounts[len(allAccounts)-1].Code
 	maxcredit := allAccounts[len(allAccounts)-1].Maxcredit
 
-	data := struct {
-		name       string
-		account_id uint
-		code       string
-		maxcredit  float64
-	}{
-		name:       name,
-		account_id: account_id,
-		code:       code,
-		maxcredit:  maxcredit,
-	}
+	var info string = fmt.Sprintf("Account Id: "+account_id+"\n"+"Naam: "+name+"\n"+"Code: "+code+"\n"+"Rood: "+"%f", account_id, maxcredit)
+
+	// data := struct {
+	// 	tekst []string
+	// }{
+	// 	tekst: info,
+	// }
 
 	render, err := template.ParseFiles("templates/account-created.html")
 
@@ -34,5 +32,5 @@ func CreationConfirmedHandler(response http.ResponseWriter, request *http.Reques
 		log.Println(err)
 	}
 
-	render.Execute(response, data)
+	render.Execute(response, info)
 }
